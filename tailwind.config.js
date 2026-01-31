0cb0a383c4248de453271a823b0506e1f96b3bc5
enottie <92582987+enottie@users.noreply.github.com>
2026-01-31 18:44:08 +0200

    Add safelist for responsive width and height utilities in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
@@ -20,8 +22,41 @@ export default {
     plugins: [forms],
 
     safelist: [
-        'w-32', 'h-32',
-        'w-48', 'h-48',
-        'w-64', 'h-64',
+        // -------------------------
+        // POSITIONING (from before)
+        // -------------------------
+        { pattern: /^(top|right|bottom|left)-\d+$/ },
+        { pattern: /^-(top|right|bottom|left)-\d+$/ },
+        { pattern: /^inset(-[xy])?-\d+$/ },
+        { pattern: /^-inset(-[xy])?-\d+$/ },
+
+        // -------------------------
+        // WIDTHS (core)
+        // -------------------------
+        // w-0, w-1, w-48, w-96, etc.
+        { pattern: /^w-\d+$/ },
+
+        // w-px
+        { pattern: /^w-px$/ },
+
+        // w-full, w-screen, w-auto, w-fit, w-min, w-max
+        { pattern: /^w-(full|screen|auto|min|max|fit)$/ },
+
+        // w-1/2, w-3/4, w-2/5, etc.
+        { pattern: /^w-\d+\/\d+$/ },
+
+        // -------------------------
+        // MAX / MIN WIDTHS
+        // -------------------------
+        { pattern: /^(min-w|max-w)-\d+$/ },
+        { pattern: /^(min-w|max-w)-(full|screen|min|max|fit)$/ },
+        { pattern: /^(min-w|max-w)-\d+\/\d+$/ },
+
+        // -------------------------
+        // ARBITRARY WIDTHS
+        // -------------------------
+        // w-[123px], w-[10rem], w-[42%]
+        { pattern: /^w-\[[^\]]+]$/ },
+        { pattern: /^(min-w|max-w)-\[[^\]]+]$/ },
     ],
 };
